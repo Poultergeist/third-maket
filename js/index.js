caf01ea969c1a73d9e29f4483a74790f2e214eea
Poultergeist <vlogatskiy3@gmail.com>
2024-08-19 17:56:11 +0300

    Added slider base

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".slider-image-container").slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        fade: true,
+        arrows: false,
+        swipe: false,
+        draggable: false
+    });
+})
