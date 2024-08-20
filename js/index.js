dec024b23a626166aaa3db023b3111ae75143380
Poultergeist <vlogatskiy3@gmail.com>
2024-08-20 11:05:58 +0300

    Added buttons, their functionality, adaptive

--- js/index.js
+++ js/index.js
@@ -1,10 +1,35 @@
 $(document).ready(function () {
-    $(".slider-image-container").slick({
-        autoplay: true,
-        autoplaySpeed: 4000,
-        fade: true,
-        arrows: false,
-        swipe: false,
-        draggable: false
-    });
+  $(".slider-image-container").slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    fade: true,
+    arrows: false,
+    swipe: false,
+    draggable: false
+  });
+
+  function updateAdaptive() {
+    if (window.innerWidth < 768) {
+      $(".slider-prev, .slider-next").hide();
+      $(".slider-image-container").slick('slickSetOption', 'swipe', true, true);
+      $(".slider-image-container").slick('slickSetOption', 'draggable', true, true);
+    }
+    else {
+      $(".slider-prev, .slider-next").show();
+      $(".slider-image-container").slick('slickSetOption', 'swipe', false, true);
+      $(".slider-image-container").slick('slickSetOption', 'draggable', false, true);
+    }
+  }
+
+  updateAdaptive();
+
+  window.onresize = updateAdaptive;
+
+  $('.slider-prev').on('click', function () {
+    $(".slider-image-container").slick('slickPrev');
+  });
+
+  $('.slider-next').on('click', function () {
+    $(".slider-image-container").slick('slickNext');
+  });
 })
