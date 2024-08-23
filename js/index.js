2833e1ea50c4562d5a3378a809ab7ba9c2598e72
Poultergeist <vlogatskiy3@gmail.com>
2024-08-23 15:29:24 +0300

    Changed Header, Menu, finalize the feedback form, changed adaptive for blocks

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   $(".slider-image-container").slick({
     autoplay: true,
+    infinite: true,
     autoplaySpeed: 4000,
     fade: true,
     arrows: false,
@@ -32,4 +33,51 @@ $(document).ready(function () {
   $('.slider-next').on('click', function () {
     $(".slider-image-container").slick('slickNext');
   });
+});
+
+const feedbackEnable = document.getElementById('feedback-link');
+const feedbackForm = document.getElementById('feedback-form-wrapper');
+const feedbackDisable = document.getElementById('close-button');
+
+feedbackEnable.addEventListener('click', function () {
+  feedbackForm.style.zIndex = 1000;
+  feedbackForm.style.opacity = 1;
+});
+
+feedbackDisable.addEventListener('click', function () {
+  feedbackForm.style.opacity = 0;
+  setTimeout(() => {
+    feedbackForm.style.zIndex = -1;
+  }, 500);
+});
+
+const submitButton = document.getElementById('submit-button');
+
+const nameField = document.getElementById('name');
+const phoneField = document.getElementById('number');
+const emailField = document.getElementById('email');
+const messageField = document.getElementById('message');
+
+submitButton.addEventListener('click', function () {
+  if (!nameField.checkValidity()) {
+    alert('Name field must be from 3 to 32 characters long');
+    return;
+  }
+
+  if (!phoneField.checkValidity()) {
+    alert('Phone number must be like +38(XXX)XXX-XX-XX (digits only)');
+    return;
+  }
+
+  if (!emailField.checkValidity()) {
+    alert('Email must be like example@mail.com');
+    return;
+  }
+
+  if (!messageField.checkValidity()) {
+    alert('Message cannot be empty');
+    return;
+  }
+
+  alert('Your message was delivered')
 })
